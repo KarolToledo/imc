e25514b2433cca335c5a7338df896534a633608c
karoltoledo <karolinatoledo2010@gmail.com>
2023-07-28 10:15:30 -0300

    refatoracao de codigo jaascript

--- src/js/index.js
+++ src/js/index.js
@@ -11,30 +11,9 @@ function calcularIMC() {
 
   // Exibe o resultado do IMC no elemento de saída
   let resultadoElement = document.getElementById("resultado");
-
-  switch (resulta.doElement) {
-    case (imc < 18.5):
-      console.log("Seu IMC é: " + imc + " - Abaixo do peso ideal")
-      break
-    case (imc >= 18.5 && imc < 25):
-      console.log("Seu IMC é: " + imc + " - Peso ideal")
-      break
-    case (imc >= 25 && imc < 30):
-      console.log("Seu IMC é: "+ imc +" - Sobrepeso")
-      break
-    case (imc >=30 && imc < 35):
-      console.log("Seu IMC é: "+ imc +"Obesidade grau I")
-      break
-    case (imc >= 35 && imc < 40):
-      console.log("Seu IMC é: "+ imc +"Obesidade grau II")
-      break
-    default:
-      console.log("Seu IMC é: "+ imc +"Obesidade grau III")
-  }
-
-  /*
+  
    if (imc < 18.5) {
-     resultadoElement.innerHTML = "Seu IMC é: " + imc + " - Abaixo do peso ideal";
+    resultadoElement.innerHTML = "Seu IMC é: " + imc + " - Abaixo do peso ideal";
    } else if (imc >= 18.5 && imc < 25) {
      resultadoElement.innerHTML = "Seu IMC é: " + imc + " - Peso ideal";
    } else if (imc >= 25 && imc < 30) {
@@ -45,6 +24,7 @@ function calcularIMC() {
      resultadoElement.innerHTML = "Seu IMC é: " + imc + " - Obesidade grau II";
    } else {
      resultadoElement.innerHTML = "Seu IMC é: " + imc + " - Obesidade grau III";
-  }*/
+  }
+
 }
  
